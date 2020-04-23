8c4a0026d4157edb9c9f1ca65166f87dda23c217
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2020-04-22 23:59:13 -0400

    as

---
+++ CirrusCircuit/Assets/CirrusCircuit/World/Objects/Networking/NetworkController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cirrus.Circuit.World.Objects
+{
+    public class ObjectNetworkController
+    {
+        [SerializeField]
+        public Mirror.NetworkIdentity _netIdentity;
+
+        [SerializeField]
+        public Mirror.NetworkTransform _netTransform;
+
+    }
+}
