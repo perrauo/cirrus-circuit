e470f683ee26378e060d0f00b46a4a92cb302fd1
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2020-04-29 12:39:29 -0400

    s

--- CirrusCircuit/Assets/CirrusCircuit/World/Objects/BaseObject.cs
+++ CirrusCircuit/Assets/CirrusCircuit/World/Objects/BaseObject.cs
@@ -228,11 +228,9 @@ namespace Cirrus.Circuit.World.Objects
 
         public void OnNextColorTimeOut()
         {
-            if (_state != State.LevelSelect)
-                return;
+            if (_state != State.LevelSelect) return;  
 
-            if (GameSession.Instance.LocalPlayers.Count == 0)
-                return;
+            if (GameSession.Instance.LocalPlayers.Count == 0) return;
 
             _nextColorIndex = _nextColorIndex + 1;
             _nextColorIndex = MathUtils.Wrap(_nextColorIndex, 0, GameSession.Instance.PlayerCount);
