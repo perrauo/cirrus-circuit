68fb8e7a03f1d49c6ae8fdc18fda6d8c6aca5443
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2020-05-30 00:54:41 -0400

    s

---
+++ CirrusCircuit/Assets/CirrusCircuit/Controls/AIController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+namespace Cirrus.Circuit.Controls.AI
+{
+    public class AIController : MonoBehaviour
+    {
+
+    }
+}
